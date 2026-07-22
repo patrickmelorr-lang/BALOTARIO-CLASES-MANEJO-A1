9cffa3641fbf75d80bca9741419403db05b4809c
Patrick J. Melo Ramos <patrickmelorr@gmail.com>
2026-07-22 09:23:18 -0500

    Add files via upload

---
+++ imagenes.js
@@ -0,0 +1,19 @@
+const imagenes = {
+  4:"img/preg-4.png", 9:"img/preg-9.png", 18:"img/preg-18.png", 28:"img/preg-28.png",
+  32:"img/preg-32.png", 33:"img/preg-33.png", 35:"img/preg-35.png", 50:"img/preg-50.png",
+  67:"img/preg-67.png", 93:"img/preg-93.png", 99:"img/preg-99.png", 100:"img/preg-100.png",
+  101:"img/preg-101.png", 102:"img/preg-102.png", 103:"img/preg-103.png", 104:"img/preg-104.png",
+  105:"img/preg-105.png", 106:"img/preg-106.png", 107:"img/preg-107.png", 108:"img/preg-108.png",
+  109:"img/preg-109.png", 110:"img/preg-110.png", 111:"img/preg-111.png", 112:"img/preg-112.png",
+  113:"img/preg-113.png", 114:"img/preg-114.png", 115:"img/preg-115.png", 116:"img/preg-116.png",
+  117:"img/preg-117.png", 118:"img/preg-118.png", 119:"img/preg-119.png", 120:"img/preg-120.png",
+  121:"img/preg-121.png", 122:"img/preg-122.png", 123:"img/preg-123.png", 124:"img/preg-124.png",
+  125:"img/preg-125.png", 126:"img/preg-126.png", 127:"img/preg-127.png", 128:"img/preg-128.png",
+  129:"img/preg-129.png", 130:"img/preg-130.png", 131:"img/preg-131.png", 132:"img/preg-132.png",
+  133:"img/preg-133.png", 134:"img/preg-134.png", 135:"img/preg-135.png", 136:"img/preg-136.png",
+  137:"img/preg-137.png", 138:"img/preg-138.png", 139:"img/preg-139.png", 140:"img/preg-140.png",
+  141:"img/preg-141.png", 142:"img/preg-142.png", 143:"img/preg-143.png", 144:"img/preg-144.png",
+  145:"img/preg-145.png", 146:"img/preg-146.png", 147:"img/preg-147.png", 148:"img/preg-148.png",
+  151:"img/preg-151.png", 181:"img/preg-181.png", 182:"img/preg-182.png", 187:"img/preg-187.png",
+  197:"img/preg-197.png", 199:"img/preg-199.png", 200:"img/preg-200.png"
+};
